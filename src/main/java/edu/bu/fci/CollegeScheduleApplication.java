83e09be920f0d5c05ff3a1db3476252aa9883979
Yahia Elakeed <elakeedyahia@johndeere.com>
2021-02-09 23:07:17 -0600

    First commit

---
+++ src/main/java/edu/bu/fci/CollegeScheduleApplication.java
@@ -0,0 +1,13 @@
+package edu.bu.fci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeScheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/bu/fci/CollegeScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.bu.fci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
